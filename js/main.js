d383f1e7111ecf0c3775bb7f35ce659901459252
Stevan Popovic <stevanpopovic5@gmail.com>
2018-04-14 17:02:55 +0100

    altered game logic in main js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,35 @@
 // console.log("User flipped " + cardOne)
 // console.log("User flipped " + cardThree)
 
-var cards = ["queen", "queen", "king", "king"];
+//var cards = ["queen", "queen", "king", "king"];
+// changed cards array to be filled with objects instead
+
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "iimages/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+
+];
+
+// we then hard to update any code that accessed this, ro reflect the new key-value pairs
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -28,8 +56,12 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-	console.log("User flipped "+cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	//used dot notation here to access rank
+	console.log("User flipped "+cards[cardId].rank); 
+	//used dot notation here to access name
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	checkForMatch();
 	
