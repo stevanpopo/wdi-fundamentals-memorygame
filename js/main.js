558e93ecbf8067116f9a362978639956eaed0782
Stevan Popovic <stevanpopovic5@gmail.com>
2018-04-17 05:34:32 +0100

    modified JS and index

--- js/main.js
+++ js/main.js
@@ -1,76 +1,70 @@
-//console.log("Up and running!");
+// Copied this code from https://git.generalassemb.ly/Darren/wdi-fundamentals-memorygame/blob/master/main.js
 
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
-
-// console.log("User flipped " + cardOne)
-// console.log("User flipped " + cardThree)
-
-//var cards = ["queen", "queen", "king", "king"];
-// changed cards array to be filled with objects instead
-
+// console.log("Up and running!");
 var cards = [
-	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
-	},
-	{
-		rank: "queen",
-		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
-	},
-	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "iimages/king-of-hearts.png"
-	},
-	{
-		rank: "king",
-		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
-	}
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
 
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   },
 ];
-
-// we then hard to update any code that accessed this, ro reflect the new key-value pairs
 
 var cardsInPlay = [];
 
-var checkForMatch = function(){
-	if (cardsInPlay.length === 2){
-		if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again.");
-		}
-	}
+function checkForMatch() {
+	//if (cardsInPlay.length == 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+    	alert("You found a match!");
+    } else {
+        alert("Sorry, try again");
+    };
+
 };
 
-// created a function called flipCard - moved code into this function
-// arrays left out of function for global scope
 
-// code alered so it uses cardId parameter
-
-var flipCard = function(cardId){
-
-	//used dot notation here to access rank
-	console.log("User flipped "+cards[cardId].rank); 
-	//used dot notation here to access name
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-
-	checkForMatch();
-	
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+    //console.log("User flipped" + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
+    //console.log(cards[cardId].suit);
+    if (cardsInPlay.length === 2){
+    	checkForMatch();
+    	//cardsInPlay = [];
+	};
 };
 
-// moved the card checking code into its own function checkForMatch
+console.log(cardsInPlay);
 
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  const cardTable = document.getElementById('game-board');
 
+  for (var i = 0; i < cards.length; i++) {
+  	var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
 
